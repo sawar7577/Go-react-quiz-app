09e86bbb8d7f8db8f5c3b6a6dc7181461f5ddfd3
b00merang <sawar.sagwal@gmail.com>
2018-09-19 03:57:28 +0530

    making signup

--- go/src/CRUD API.go
+++ go/src/CRUD API.go
@@ -1,91 +1,76 @@
 package main
 
 import (
-   "fmt"
-   "github.com/gin-contrib/cors"                        // Why do we need this package?
-   "github.com/gin-gonic/gin"
-   "github.com/jinzhu/gorm"
-   _ "github.com/jinzhu/gorm/dialects/sqlite"           // If you want to use mysql or any other db, replace this line
+	"fmt"
+
+	"github.com/gin-contrib/cors" // Why do we need this package?
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite" // If you want to use mysql or any other db, replace this line
 )
 
-var db *gorm.DB                                         // declaring the db globally
+var db *gorm.DB // declaring the db globally
 var err error
 
 type Person struct {
-   ID uint `json:"id"`
-   FirstName string `json:"firstname"`
-   LastName string `json:"lastname"`
-   City string `json:"city"`
+	ID        uint   `gorm:"primary_key" json:"id"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+	UserName  string `json:"username"`
+	Password  string `json:"password"`
+}
+
+type Quiz struct {
+	ID        uint `gorm:"primary_key" json:"id"`
+	Questions []Question
+}
+
+type Question struct {
+	ID      uint   `gorm:"primary_key" json:"id"`
+	Ques    string `json:"ques"`
+	Options []Option
+}
+
+type Option struct {
+	ID     uint `gorm:"primary_key" json:"id"`
+	option string
+	val    bool
 }
 
 func main() {
-   db, err = gorm.Open("sqlite3", "./gorm.db")
-   if err != nil {
-      fmt.Println(err)
-   }
-   defer db.Close()
+	db, err = gorm.Open("sqlite3", "./gorm.db")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+	db.AutoMigrate(&Person{})
+	fmt.Printf("idhar")
 
-   db.AutoMigrate(&Person{})
-   r := gin.Default()
-   r.GET("/people/", GetPeople)                             // Creating routes for each functionality
-   r.GET("/people/:id", GetPerson)
-   r.POST("/people", CreatePerson)
-   r.PUT("/people/:id", UpdatePerson)
-   r.DELETE("/people/:id", DeletePerson)
-   r.Use((cors.Default()))
-   r.Run(":8080")                                           // Run on port 8080
+	db.AutoMigrate(&Quiz{})
+	db.AutoMigrate(&Question{})
+	db.AutoMigrate(&Option{})
+
+	r := gin.Default()
+	r.POST("/person", CreatePerson)
+	r.Use((cors.Default()))
+	r.Run(":8080") // Run on port 8080
 }
 
-
-func DeletePerson(c *gin.Context) {
-   id := c.Params.ByName("id")
-   var person Person
-   d := db.Where("id = ?", id).Delete(&person)
-   fmt.Println(d)
-   c.Header("access-control-allow-origin", "*")
-   c.JSON(200, gin.H{"id #" + id: "deleted"})
-}
-
-func UpdatePerson(c *gin.Context) {
-   var person Person
-   id := c.Params.ByName("id")
-   if err := db.Where("id = ?", id).First(&person).Error; err != nil {
-      c.AbortWithStatus(404)
-      fmt.Println(err)
-   }
-   c.BindJSON(&person)
-   db.Save(&person)
-   c.Header("access-control-allow-origin", "*") // Why am I doing this? Find out. Try running with this line commented
-   c.JSON(200, person)
+func CheckUsername(c *gin.Context) {
+	username := c.Params.ByName("UserName")
+	rows := db.Where("UserName = ?", username).Find(&Person{})
+	defer rows.Close()
+	if rows != nil {
+		c.JSON(200, gin.H{"success": "true"})
+	} else {
+		c.JSON(200, gin.H{"success": "false"})
+	}
 }
 
 func CreatePerson(c *gin.Context) {
-   var person Person
-   c.BindJSON(&person)
-   db.Create(&person)
-   c.Header("access-control-allow-origin", "*") // Why am I doing this? Find out. Try running with this line commented
-   c.JSON(200, person)
-}
-
-func GetPerson(c *gin.Context) {
-   id := c.Params.ByName("id")
-   var person Person
-   if err := db.Where("id = ?", id).First(&person).Error; err != nil {
-      c.AbortWithStatus(404)
-      fmt.Println(err)
-   } else {
-      c.Header("access-control-allow-origin", "*") // Why am I doing this? Find out. Try running with this line commented
-      c.JSON(200, person)
-   }
-}
-
-func GetPeople(c *gin.Context) {
-   var people []Person
-   if err := db.Find(&people).Error; err != nil {
-      c.AbortWithStatus(404)
-      fmt.Println(err)
-   } else {
-      c.Header("access-control-allow-origin", "*") // Why am I doing this? Find out. Try running with this line commented
-      c.JSON(200, people)
-   }
+	var person Person
+	c.BindJSON(&person)
+	db.Create(&person)
+	c.Header("access-control-allow-origin", "*") // Why am I doing this? Find out. Try running with this line commented
+	c.JSON(200, person)
 }
